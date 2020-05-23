2834d5e5ab7d152e50ab36f1ab02b9de262009fc
Alper Hankendi <ahankendi@gmail.com>
2020-05-23 23:38:17 +0300

    first commit

---
+++ web/auth0-variables.js
@@ -0,0 +1,5 @@
+var AUTH0_CLIENT_ID='6YJclYqwVKQcEITGUT9OxZL0IG6MnPjb';
+var AUTH0_DOMAIN='ahankendi.auth0.com';
+var AUTH0_AUDIENCE = 'http://localhost:3000';
+var AUTH0_CALLBACK_URL = location.href;
+
